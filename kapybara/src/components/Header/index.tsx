79afa673575830844ef7acc1a4e5ec7928dc7cb2
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2025-02-12 23:07:59 +0530

    [UI] Initialization

---
+++ kapybara/src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { Button } from "../ui/button";
+import { SidebarTrigger } from "../ui/sidebar";
+
+function Header() {
+  return (
+    <div className="sticky top-0 flex w-full gap-8 p-2 items-center h-16 bg-white shadow-md z-10">
+      <SidebarTrigger className="p-4" />
+      Kapybara - Your partner in crime
+      <div className="ml-auto flex gap-4">
+        <Button>Sign-up</Button>
+        <Button>Log-in</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;

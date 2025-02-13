845bbbea19ce1af23be527a246dc1afce087cbd7
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2025-02-13 22:14:21 +0530

    [MISC] Disabled eslint for builds

---
+++ src/types/loader.d.ts
@@ -0,0 +1,10 @@
+// filepath: /c:/Users/Kowshik/Documents/GitHub/Kapybara-assignment/src/types/custom-elements.d.ts
+declare namespace JSX {
+    interface IntrinsicElements {
+      'l-grid': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        size?: string;
+        speed?: string;
+        color?: string;
+      };
+    }
+  }

1579d22b2904be919dd4724fdf32787882dfe980
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2025-02-13 22:20:28 +0530

    [MISC] Removed Loader
    eslint please sstop

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import { useLoadingStore } from "@/stores/loading-store";
-import { grid } from "ldrs";
 
-grid.register();
 
 const Loader = () => {
   const isLoading = useLoadingStore((state) => state.isLoading);
@@ -12,7 +10,6 @@ const Loader = () => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50 z-50">
-      <l-grid size="120" speed="2.0" color="white" />
     </div>
   );
 };

b16a9eb5a2002fb24ed9308512adcfa5ab521133
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2025-02-15 01:15:50 +0530

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ function DashboardPage() {
   return (
     <div className="h-screen flex items-center justify-center">
       <p className="text-4xl font-bold mx-16">
-        Unfortunately, due to time constraints and caffeine overload, I couldn't
+        Unfortunately, due to time constraints and caffeine overload, I couldn&apos;t
         get the dashboard done
       </p>
     </div>
